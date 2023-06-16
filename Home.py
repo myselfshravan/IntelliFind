4712d769973419488ec70281502db426fbb9fc6b
yashp <yashgupta.yg856@gmail.com>
2023-06-16 06:58:29 +0530

    v1

--- Home.py
+++ Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import requests
 
 st.write('''## Welcome to IntelliFind!''')
 
@@ -8,7 +9,7 @@ st.sidebar.write('''## Options''')
 
 option = st.sidebar.selectbox(
     'What would you like to do?',
-    ('Home', 'Lost', 'Found', 'About Us')
+    ('Home', 'Lost', 'Found', 'About Us',"Get")
 )
 
 if option == 'Home':
@@ -28,11 +29,55 @@ elif option == 'Lost':
         input = st.text_input("Please specify the category of your item")
 
 elif option == 'Found':
-    st.subheader("Please add your found item here")
-    img_in = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
-    if img_in is not None:
-        st.write(img_in)
-        st.image(img_in, caption="Uploaded Image", use_column_width=True)
+    uploaded_file = st.file_uploader("Choose an image file", type=["png", "jpg", "jpeg"], accept_multiple_files=False)
+
+        # Display the uploaded image
+    st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+    submit_button = st.button("Send POST Request")
+    if submit_button:
+            try:
+                # Create the payload
+                payload = {"image": uploaded_file}
+
+                # Send the POST request with the image file as multipart form-data
+                response = requests.post("https://in35zed2ta.execute-api.us-east-1.amazonaws.com/api/admin/found", files=payload)
+
+                # Check the response status code
+                if response.status_code == 200:
+                    st.success("POST request sent successfully.")
+                else:
+                    st.error(f"Error sending POST request. Status code: {response.status_code}")
+            except Exception as e:
+                st.error(f"Error sending POST request: {str(e)}")
+ 
+elif option == 'Get':
+                 
+    submit_button1 = st.button("Send GET Request")
+    if submit_button1:
+        try:
+        # Send the GET request to retrieve image data
+            response = requests.get("https://in35zed2ta.execute-api.us-east-1.amazonaws.com/api/client/getimages/843eab46-d153-4710-999f-06e5048cd1eb")
+            st.write(response)
+        # Check the response status code
+            if response.status_code == 200:
+                response_data = response.json()
+                all_images = response_data["all_images"]
+            
+                for image_data in all_images:
+                     pid = image_data["pid"]
+                     image = image_data["filenames"]
+
+                     st.write("Image:", image)
+                     st.image(image)
+            
+                     st.success("GET request sent successfully.")
+            else:
+                    st.error(f"Error sending GET request. Status code: {response.status_code}")
+        except Exception as e:
+            st.error(f"Error sending GET request: {str(e)}")
+
+    
+    
 
 hide_streamlit_style = """
                     <style>
